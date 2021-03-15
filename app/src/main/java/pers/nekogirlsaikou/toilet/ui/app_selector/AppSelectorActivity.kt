16a51060b83886319a1fda060eaefc5e8ef2a9d8
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-03-15 20:13:00 +0800

    Fix AppSelectorActivity crash

--- app/src/main/java/pers/nekogirlsaikou/toilet/ui/app_selector/AppSelectorActivity.kt
+++ app/src/main/java/pers/nekogirlsaikou/toilet/ui/app_selector/AppSelectorActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import pers.nekogirlsaikou.toilet.databinding.ActivityAppSelectorBinding
+import pers.nekogirlsaikou.toilet.utils.RealmUtil
 
 class AppSelectorActivity : AppCompatActivity() {
     lateinit var binding:ActivityAppSelectorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        RealmUtil.initRealm(this)
         binding = ActivityAppSelectorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 

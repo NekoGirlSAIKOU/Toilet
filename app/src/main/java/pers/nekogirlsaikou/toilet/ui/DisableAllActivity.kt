ca79f4ff5ab9f2438a1fb60c646b38bd808c7a3d
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-03-15 14:24:40 +0800

    Add static shortcuts disable all

--- app/src/main/java/pers/nekogirlsaikou/toilet/ui/DisableAllActivity.kt
+++ app/src/main/java/pers/nekogirlsaikou/toilet/ui/DisableAllActivity.kt
@@ -2,15 +2,17 @@ package pers.nekogirlsaikou.toilet.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import pers.nekogirlsaikou.toilet.utils.RealmUtil
 import pers.nekogirlsaikou.toilet.utils.disable
 import pers.nekogirlsaikou.toilet.utils.isInToilet
 
-class DisableAllActivity: AppCompatActivity() {
+class DisableAllActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        RealmUtil.initRealm(this)
         packageManager.getInstalledPackages(0)
-                .filter { it.isInToilet() && it.applicationInfo.enabled }
-                .forEach { it.disable() }
+            .filter { it.isInToilet() && it.applicationInfo.enabled }
+            .forEach { it.disable() }
         finish()
     }
 }

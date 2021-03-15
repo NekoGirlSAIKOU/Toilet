b124692675cbc873cc82d68caf8d06978134bcc7
NekoGirlSAIKOU <nekogirlsaikou@outlook.com>
2021-03-15 21:05:59 +0800

    Fix crash when trying to launch a nonexist App

--- app/src/main/java/pers/nekogirlsaikou/toilet/ui/LaunchAppActivity.kt
+++ app/src/main/java/pers/nekogirlsaikou/toilet/ui/LaunchAppActivity.kt
@@ -1,11 +1,13 @@
 package pers.nekogirlsaikou.toilet.ui
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import pers.nekogirlsaikou.toilet.utils.enable
 import pers.nekogirlsaikou.toilet.utils.launch
+import kotlin.system.exitProcess
 
 class LaunchAppActivity : AppCompatActivity() {
 
@@ -13,11 +15,14 @@ class LaunchAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val packageName = intent.extras?.getString("packageName")
         if (packageName != null){
-            val packageInfo = packageManager.getPackageInfo(packageName,0)
-            if (!packageInfo.applicationInfo.enabled){
-                packageInfo.enable()
+            try {
+                val packageInfo = packageManager.getPackageInfo(packageName,0)
+                if (!packageInfo.applicationInfo.enabled){
+                    packageInfo.enable()
+                }
+                packageInfo.launch(this)
+            } catch (e: PackageManager.NameNotFoundException){
             }
-            packageInfo.launch(this)
         }
         finish()
     }
